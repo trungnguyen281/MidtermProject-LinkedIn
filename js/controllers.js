15224459f617c33060163d670bdfc0465faaea5e
trungnguyen281 <ndtrunga195@gmail.com>
2016-04-27 16:49:39 +0700

    Update json
    Post, exp

--- js/controllers.js
+++ js/controllers.js
@@ -4,5 +4,17 @@ LinkedInApp.controller('LinkedInCtrl', function($scope, $http){
 	$http.get('database/profile.json').success(function(data) {
 		$scope.profile = data[0];
 	});
+
+	$http.get('database/post.json').success(function(data) {
+		$scope.posts = data;
+	});
+
+	$http.get('database/summary.json').success(function(data) {
+		$scope.summary = data[0];
+	});
+
+	$http.get('database/experience.json').success(function(data) {
+		$scope.experiences = data;
+	});
 });
 
