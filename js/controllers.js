b6ecb177127a97e9c8b3f8bc56e0cb6f3971c4cb
trungnguyen281 <ndtrunga195@gmail.com>
2016-05-06 16:41:29 +0700

    Update
    Chỉnh sửa, hoàn thiện

--- js/controllers.js
+++ js/controllers.js
@@ -5,78 +5,111 @@ LinkedInApp.run(function(editableOptions) {
 });
 
 LinkedInApp.controller('LinkedInCtrl', function($scope, $http){
-	$http.get('database/profile.json').success(function(data) {
-		$scope.profile = data[0];
-	});
+  $http.get('database/profile.json').success(function(data) {
+    $scope.profile = data[0];
+  });
 
-	$http.get('database/post.json').success(function(data) {
-		$scope.posts = data;
-	});
+  $http.get('database/post.json').success(function(data) {
+    $scope.posts = data;
+  });
 
-	$http.get('database/summary.json').success(function(data) {
-		$scope.summary = data[0];
-	});
+  $http.get('database/summary.json').success(function(data) {
+    $scope.summary = data;
+  });
 
-	$http.get('database/experience.json').success(function(data) {
-		$scope.experiences = data;
-	});
+  $http.get('database/experience.json').success(function(data) {
+    $scope.experiences = data;
+  });
 
-	$http.get('database/project.json').success(function(data) {
-		$scope.projects = data;
-	});
+  $http.get('database/project.json').success(function(data) {
+    $scope.projects = data;
+  });
 
-	$http.get('database/skill.json').success(function(data) {
-		data.sort(function(a, b) {
- 		return parseFloat(b.point) - parseFloat(a.point);
-		});
+  $http.get('database/skill.json').success(function(data) {
+    data.sort(function(a, b) {
+    return parseFloat(b.point) - parseFloat(a.point);
+    });
 
-		$scope.topskills = data.slice(0,6);
-		$scope.otherskills = data.slice(6,data.length);
-	});
+    $scope.topskills = data.slice(0,6);
+    $scope.otherskills = data.slice(6,data.length);
+  });
 
-	$http.get('database/education.json').success(function(data) {
-		$scope.educations = data;
-	});
+  $http.get('database/education.json').success(function(data) {
+    $scope.educations = data;
+  });
 
-	$scope.addExperience = function() {
-   	 	$scope.inserted = {
-      		job: $scope.job,
-      		companyname: $scope.companyname,
-      		logocompany: $scope.logocompany,
-      		time: $scope.time,
-      		locate: $scope.locate,
-      		detail: $scope.detail
-    	};
-    	$scope.experiences.push($scope.inserted);
-  	};
+  $scope.addSummary = function() {
+      $scope.inserted = {
+          content: $scope.summarydetail
+      };
+      $scope.summarydetail = "";
 
-	$scope.addProject = function() {
-   	 	$scope.inserted = {
-      		projectname: $scope.projectname,
-      		knowledge: [$scope.knowledge1, $scope.knowledge2, $scope.knowledge3] ,
-      		time: $scope.time,
-      		detail: $scope.detail
-    	};
-    	$scope.projects.push($scope.inserted);
-  	};
+      $scope.summary.push($scope.inserted);
+    };
 
-  	$scope.addSkill = function() {
-   	 	$scope.inserted = {
-      		point: '0',
-      		name: $scope.skillname
-    	};
-    	$scope.otherskills.push($scope.inserted);
-    	$scope.skillname = '';
-  	};
+  $scope.addExperience = function() {
+      $scope.inserted = {
+          job: $scope.job,
+          companyname: $scope.companyname,
+          logocompany: $scope.logocompany,
+          time: $scope.time,
+          locate: $scope.locate,
+          detail: $scope.detail
+      };
 
-  	$scope.addEducation = function() {
-   	 	$scope.inserted = {
-      		schoolname: $scope.schoolname,
-      		logoschool: $scope.logoschool,
-      		major: $scope.major,
-      		time: $scope.time
-    	};
-    	$scope.educations.push($scope.inserted);
-  	};
+      $scope.job = "";
+      $scope.companyname = "";
+      $scope.logocompany = "";
+      $scope.time = "";
+      $scope.locate = "";
+      $scope.detail = "";
+
+      $scope.experiences.push($scope.inserted);
+    };
+
+  $scope.addProject = function() {
+      $scope.inserted = {
+          projectname: $scope.projectname,
+          knowledge: [$scope.knowledge1, $scope.knowledge2, $scope.knowledge3] ,
+          time: $scope.time,
+          detail: $scope.detail
+      };
+
+      scope.projectname = "";
+      $scope.knowledge1 = "";
+      $scope.knowledge2 = "";
+      $scope.knowledge3 = "";
+      $scope.time = "";
+      $scope.detail = "";
+      
+      $scope.projects.push($scope.inserted);
+    };
+
+    $scope.addSkill = function() {
+      $scope.inserted = {
+          point: '0',
+          name: $scope.skillname
+      };
+
+      $scope.skillname = "";
+
+      $scope.otherskills.push($scope.inserted);
+    };
+
+    $scope.addEducation = function() {
+      $scope.inserted = {
+          schoolname: $scope.schoolname,
+          logoschool: $scope.logoschool,
+          major: $scope.major,
+          time: $scope.time
+      };
+
+      $scope.schoolname = "";
+      scope.logoschool = "";
+      $scope.major = "";
+      $scope.time = "";
+
+      $scope.educations.push($scope.inserted);
+    };
 });
 
